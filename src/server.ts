db1ee9199e0a6bcc11f16f2bb23ccdd4f8217784
EmanuelJorge3MA <emanuel.jorge.130@gmail.com>
2023-07-13 22:55:19 +0100

    fix: host render

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { app } from './app'
 import { env } from './env'
 
+const host = 'RENDER' in process.env ? '0.0.0.0' : 'localhost'
+
 // Run the server!
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ host, port: env.PORT }).then(() => {
   console.log('HTTP Server Running...')
 })
